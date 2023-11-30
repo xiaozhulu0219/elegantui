2a2373279056bc795cfa75e004ee4daad308a7ab
小猪露 <317879181@qq.com>
2023-12-01 01:01:55 +0800

    封装icon图标

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,23 @@
+<script setup >
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+import elgProps from "./props"
+import "@/style/components/Icon.scss"
+library.add(fas)
+defineOptions({
+    name: 'elg-icon',
+})
+const prop = defineProps(elgProps);
+</script>
+
+<template>
+    <i class="elg-icon" :class="prop.type?`elg-icon-${prop.type}`:''"
+    >
+        <font-awesome-icon v-bind="prop"   />
+    </i>
+    
+
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from "./router"
 import "./style/index.scss"
+
+// 引入公共css
+import "@/assets/base.css"
+// 引入icon组件
+import Icon from "@/components/Icon/Icon.vue";
 const app = createApp(App)
+app.component('Elg-Icon',Icon)
 app.use(router)
 app.mount('#app')
 
