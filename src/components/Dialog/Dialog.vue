8d97ba3d8834bbaca5b63efc0288a746e3f1b7ea
小猪露 <317879181@qq.com>
2023-12-06 22:31:40 +0800

    封装对话框

---
+++ src/components/Dialog/Dialog.vue
@@ -0,0 +1,54 @@
+<script setup >
+import dialogProps from "./props"
+import "@/style/components/Dialog.scss"
+defineOptions({
+    name: 'Elg-Dialog'
+})
+const emit = defineEmits(['close', 'confirm'])
+const props = defineProps(dialogProps)
+const handleClose = () => {
+    //取消弹框
+    emit('close')
+}
+const handleClick = () => {
+    emit('confirm')
+}
+</script>
+
+<template>
+    <Teleport to="body">
+        <!-- 背景 -->
+        <transition name="fade" appear >
+        <div class="elg-dialog-container" v-if="props.visible"   :style="{'--elg-dialog-width':props.width}">
+          
+
+                <div class="elg-dialog" :style="{ width: props.width, top: props.top }">
+                    <div class="elg-dialog-title">
+                        <div class="elg-dialog-title-left">
+                            {{ props.title }}
+                        </div>
+                        <div class="elg-dialog-title-right">
+                            <Elg-Icon icon="fa-times" @click="handleClose" />
+                        </div>
+                    </div>
+                    <div class="elg-dialog-body">
+                        <slot name="default"></slot>
+                    </div>
+                    <div class="elg-dialog-footer" v-if="props.footer && !$slots.footer ">
+                        <Elg-Button @click="handleClose">取消</Elg-Button>
+
+                        <Elg-Button type="primary" @click="handleClick">确定</Elg-Button>
+                    </div>
+                    <div class="elg-dialog-footer-self" v-if="$slots.footer ">
+                        <slot name="footer"></slot>
+                    </div>
+                </div>
+
+        </div>
+    </transition>
+
+
+    </Teleport>
+</template>
+
+<style scoped></style>
---
+++ src/components/Dialog/props.js
@@ -0,0 +1,23 @@
+export default {
+    title:{
+        type:String,
+        default:"提示"
+    },
+    width:{
+        type:String,
+        default:'30vw'
+    },
+    top:{
+        type:String,
+        default:'15vh'
+    },
+    visible:{
+        type:Boolean,
+        default:false
+
+    },
+    footer:{
+        type:Boolean,
+        default:true
+    }
+}
